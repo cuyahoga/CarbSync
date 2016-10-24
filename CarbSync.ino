a6e800b8971616f22ca32b1aa31a817999707e5a
cuyahoga <stu@cuyahoga.co.uk>
2016-10-24 16:51:00 +0100

    Play about with an LCD as an alternate to the OLED

--- CarbSync.ino
+++ CarbSync.ino
@@ -7,21 +7,51 @@
 *********************************************************************/
 
 #include <Wire.h>
-#include <Adafruit_GFX.h>        // https://github.com/adafruit/Adafruit-GFX-Library
-#include <Adafruit_SSD1306.h>    // https://github.com/adafruit/Adafruit_SSD1306
 #include <Timer.h>               // https://github.com/JChristensen/Timer
+#include "RunningAverage.h"      // http://playground.arduino.cc/Main/RunningAverage
 
-#define OLED_DC 11
-#define OLED_CS 12
-#define OLED_CLK 10
-#define OLED_MOSI 9
-#define OLED_RESET 13
-Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+RunningAverage lcdRefreshRate(10);
+
+// ===============================================================
+// Adafruit ST7565 128x64 GLCD
+// ===============================================================
+  #define SCREEN_ST7565
+  #include "JeeLib.h"              // https://github.com/jcw/jeelib
+  #include <GLCD_ST7565.h>         // https://github.com/cuyahoga/glcdlib
+  #include "utility/font_clR6x8.h"
+  #define PIN_RED       3  // Red cathode
+  #define PIN_GREEN     5  // Green cathode
+  #define PIN_BLUE      6  // Blue cathode
+  #define LCD_BLACK     1
+  #define LCD_WHITE     0
+  #define LCD_RED       0, 255, 255
+  #define LCD_GREEN     255, 0, 255
+  #define LCD_BLUE      255, 255, 0
+  GLCD_ST7565           glcd;
+  // Backlight variables for PWM
+  uint8_t redLEDValue   = 255;
+  uint8_t greenLEDValue = 255;
+  uint8_t blueLEDValue  = 0;
+  
+// ===============================================================
+// Adafruit SSD1306 128x32 OLED
+// ===============================================================
+/*
+  #define SCREEN_SSD1306
+  #include <Adafruit_GFX.h>        // https://github.com/adafruit/Adafruit-GFX-Library
+  #include <Adafruit_SSD1306.h>    // https://github.com/adafruit/Adafruit_SSD1306
+  #define OLED_DC         11
+  #define OLED_CS         12
+  #define OLED_CLK        10
+  #define OLED_MOSI       9
+  #define OLED_RESET      13
+  Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+  #if (SSD1306_LCDHEIGHT != 32)
+  #error("Height incorrect, please fix Adafruit_SSD1306.h!");
+  #endif
+*/
+
 Timer t;
-
-#if (SSD1306_LCDHEIGHT != 32)
-#error("Height incorrect, please fix Adafruit_SSD1306.h!");
-#endif
 
 #define MODE_SUMMARY 1
 #define MODE_GAUGE   2
@@ -37,23 +67,45 @@ uint8_t carbPressure[CARB_INPUTS] = {MIN_PRESSURE, MAX_PRESSURE, MIN_PRESSURE, M
 uint8_t adj[CARB_INPUTS] = {2, 4, 6, 8};
 
 uint8_t mode;
+char   buffer[10]; 
 
 void setup()   {                
   Serial.begin(9600);
 
-  // int tickEvent = t.every(20, refreshGaugeDisplay);
+  int tickEvent = t.every(200, refreshGaugeDisplay);
     
+#if defined(SCREEN_SSD1306)
   // Set up the 128x32 OLED
   display.begin(SSD1306_SWITCHCAPVCC);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  
+#endif
+#if defined(SCREEN_ST7565)
+  glcd.begin(0x19);
+  glcd.setFont(font_clR6x8);
+
+  // Initialise the backlight
+  pinMode(PIN_RED, OUTPUT);   
+  pinMode(PIN_GREEN, OUTPUT);
+  pinMode(PIN_BLUE, OUTPUT);
+  setBacklight(LCD_BLUE);
+#endif
+
   mode = MODE_SUMMARY;
 }
 
 void loop() {
-  t.update();
+  //t.update();
+  
+  long t = millis();
+  refreshGaugeDisplay();
+  uint8_t refresh = millis() - t;
+  Serial.print("refresh: ");Serial.print(refresh);Serial.print("  avg : ");Serial.print(lcdRefreshRate.getAverage() * 1.1);Serial.print(" : ");Serial.println((int)((lcdRefreshRate.getAverage() * 1.1) + 0.5f));
+  lcdRefreshRate.addValue(refresh);
+  
+  delay(10);
+
 }
 
 void refreshGaugeDisplay() 
@@ -75,17 +127,29 @@ void refreshGaugeDisplay()
   pRange = pHigh - pLow;
 
   // Display each of the values relative to the current range
+#if defined(SCREEN_SSD1306)
   display.clearDisplay();
+#endif  
+#if defined(SCREEN_ST7565)
+    glcd.fillRect(0, 0, 128, 32, LCD_WHITE); 
+#endif
   for (uint8_t i = 0; i < CARB_INPUTS; i++) {
     renderGaugeRow(i, carbPressure[i], pLow, pRange);
   }
+#if defined(SCREEN_SSD1306)
   display.display();
+#endif
+#if defined(SCREEN_ST7565)
+  glcd.refresh();
+#endif
 
   if (TEST_MODE)
   {
     // If all values match, hang around for a while
     if (pRange == 0) {
+      setBacklight(LCD_GREEN);
       delay(10000);
+      setBacklight(LCD_BLUE);
     }
   }
 }
@@ -105,11 +169,32 @@ void renderGaugeRow(uint8_t row, uint8_t val, uint8_t low, uint8_t range) {
   // Map the row number to a Y co-ordinate
   row = (row * 8) + 1;
   // Show the current value
+#if defined(SCREEN_SSD1306)
   display.setCursor(0, row);
   display.print(val);
+#endif
+#if defined(SCREEN_ST7565)
+  sprintf(buffer, "%d", val);
+  glcd.drawString(2, row, buffer);
+#endif
   // Show a slider representing the current value for the row
   uint8_t rel = map((range == 0 ? 1 : val - low), 0, (range == 0 ? 2 : range), 0, 90);
+#if defined(SCREEN_SSD1306)
   display.fillRect(21 + rel, row, 8, 7, WHITE);
-  display.fillRect(21 + rel + 10, row, 8, 7, WHITE);  
+  display.fillRect(21 + rel + 10, row, 8, 7, WHITE);
+#endif
+#if defined(SCREEN_ST7565)
+  glcd.fillRect(21 + rel, row, 8, 7, LCD_BLACK);
+  glcd.fillRect(21 + rel + 10, row, 8, 7, LCD_BLACK);
+#endif
 }
+
+void setBacklight(uint8_t r, uint8_t g, uint8_t b) {
+#if defined(SCREEN_ST7565)
+  analogWrite(PIN_RED,   r); 
+  analogWrite(PIN_GREEN, g);
+  analogWrite(PIN_BLUE,  b);
+#endif
+}
+
 
